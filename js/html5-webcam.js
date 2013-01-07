fd3668d2d05505ef0543df38c931ee94a8f4ead6
Roman Konstantinovich <rmnx.rc@gmail.com>
2013-01-07 22:11:35 +0600

    modal window is centered

--- js/html5-webcam.js
+++ js/html5-webcam.js
@@ -17,33 +17,63 @@ navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia
     
     $.fn.html5WebCam = function(options) {
         var $this = $(this),
+            stream,
             o = $.extend({
                 NOT_SUPPORT_FEATURE: 'this browser does not support webcam capturing',
                 CAMERA_NOT_FOUND: 'camera not found on this device',
                 CLICK_TO_PAUSE: 'click to play/pause',
                 TAKE_SNAPSHOT: 'take snapshot',
                 CANCEL: 'cancel',
-
+                modal_class: 'modal',
             },options),
-            $modal = $(
-                "<div><div><video autoplay title='pause'></div> <input type='button' value='{snapshot}'/><input type='button' value='{cancel}'/></div>"
-                .supplant({pause: o.CLICK_TO_PAUSE, snapshot: o.TAKE_SNAPSHOT, cancel: o.CANCEL })
-            ),
+            modal =
+                "<div class='{modal_class}' style='position:fixed; display: none;'>" +
+                    "<div><video autoplay title='{pause}'></div>" +
+                    "<input type='button' name='snapshot' value='{snapshot}'/>" +
+                    "<input type='button' name='cancel' value='{cancel}'/>" +
+                "</div>",
+            $modal = $(modal.supplant({
+                pause: o.CLICK_TO_PAUSE, 
+                snapshot: o.TAKE_SNAPSHOT, 
+                cancel: o.CANCEL,
+                modal_class: o.modal_class
+            })),
             $video = $modal.find('video'),
             video = $video[0],
-            $btns = $modal.find('input[type=button]'),
-            $btn_snapshot = $($btns[0]),
-            $btn_cancel = $($btns[1]);
+            $btn_snapshot = $modal.find('input[name=snapshot]'),
+            $btn_cancel = $modal.find('input[name=cancel]');
 
         $('body').append($modal);
 
         $video.on('click', function() { video.paused ? video.play() : video.pause(); });
+        $video.one('play', function() { 
+            $modal
+                .css('left', $(window).width()/2 + 'px')
+                .css('margin-left', -$modal.width()/2 + 'px');
+        });
+
+        $btn_cancel.on('click', function() {
+            stream.stop();
+            $modal.hide();
+        });
+
+        $btn_snapshot.on('click', function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+            $this.trigger('webcam_snapshot', canvas.toDataURL());
+            stream.stop();
+            $modal.hide();
+        });
 
         $this.on('click', function() {
             if (!navigator.getUserMedia) { alert(o.NOT_SUPPORT_FEATURE); return false; }
 
-            navigator.getUserMedia && navigator.getUserMedia({video: true, audio: true}, function(stream) {
+            navigator.getUserMedia && navigator.getUserMedia({video: true, audio: true}, function(_stream) {
+                stream = _stream;
                 video.src = window.URL.createObjectURL(stream);
+                $modal.show();
             }, function() { alert(o.CAMERA_NOT_FOUND); });
         });
     };
